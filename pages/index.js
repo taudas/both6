72ea24d70689040680e3d340ee75dff21fce0250
taudas <taudas@gmail.com>
2022-12-28 21:39:24 -0500

    added second player

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,27 @@ export default function Index() {
   <video   id="my-video"
     class="video-js"
     controls
-        poster="/wcbn.png"
+        poster="/naiv.png"
     preload="auto"
-    fullscreen="true"
    // width="100%"
   //  height="264"
     data-setup="{}">
     <source src="https://beanball.wcbn.org:8443/wcbn6.mp3" type="video/mp4" >
     </source>
     </video>
+
+  <video   id="my-video"
+    class="video-js"
+    controls
+        poster="/wcbn.png"
+    preload="auto"
+ width="640"
+  //  height="264"
+    data-setup="{}">
+    <source src="https://drive.google.com/file/d/1ulUVW1VbyprENI9t8xIW_BFvsPWcdA_O" type="video/mp4" >
+    </source>
+    </video>
+
     </center>
   <Script src="https://vjs.zencdn.net/7.20.3/video.min.js"></Script>
 </div>
