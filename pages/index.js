8e81bcfafbef506fd81cf7a9baffbe316a0b5c12
taudas <taudas@gmail.com>
2022-12-30 14:01:54 -0500

    background color

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Index() {
 <center>
 
   <video   id="my-video"
-    class="video-js"
+    className="video-js"
     controls
         poster="/flou.png"
     preload="auto"
@@ -23,25 +23,25 @@ export default function Index() {
     </video>
 <h1>we got both kinds</h1>
 
-  <video   id="my-video"
-    class="video-js"
+  <video   id="my-video2"
+    className="video-js"
     controls
         poster="/naiv.png"
     preload="auto"
-// width="640"
-  //  height="264"
+width="640"
+    height="320"
     data-setup="{}">
     <source src="https://bothkinds7.web.app/SpecialEd-Story_Of_The_Naivete.mp3" type="video/mp4" >
     </source>
     </video>
 
-  <video   id="my-video"
-    class="video-js"
+  <video   id="my-video3"
+    className="video-js"
     controls
         poster="/bird.png"
     preload="auto"
-// width="640"
-  //  height="264"
+width="640"
+    height="264"
     data-setup="{}">
     <source src="https://bothkinds7.web.app/SpecialEd-Birdsongs.mp3" type="video/mp4" >
     </source>
@@ -53,7 +53,7 @@ export default function Index() {
   <Script src="https://vjs.zencdn.net/7.20.3/video.min.js"></Script>
 <center>
 <h1>
-<a  href="https://dangling-ganglion.bandcamp.com/">Welcome to Both Kinds - Visit Dangling Ganglion on Bandcamp </a>
+<a href="https://dangling-ganglion.bandcamp.com/">Visit Dangling Ganglion on Bandcamp</a>
 </h1>
 </center>
 </div>
