c409537db9499a6b55c635d38e98a311f82e6aca
taudas <taudas@gmail.com>
2023-01-13 15:22:33 -0500

    added second clear leader  Please enter the commit message for your

--- pages/index.js
+++ pages/index.js
@@ -205,9 +205,13 @@ SpacialEd20060629
   >
     <h3 className="vertical-timeline-element-title">CLEAR LEADER</h3>
     <h4 className="vertical-timeline-element-subtitle">made for Ann Arbor Film Festival</h4>
+<p><h5>In the lobby of The Michigan Theater during the week of the festival,                                                      
+the audience is invited to contribute their own art by way of colored                                                      
+markers onto a reel of clear leader film. </h5>
+</p>
+
     <p>
-      <Popup trigger={<button className="button">festival patrons were asked to draw on clear film media to be shown during the finale
-      <br></br>these are the soundtracks</button>} modal>
+      <Popup trigger={<button className="button">SOUNDTRACKS</button>} modal>
     <center>
     
     
@@ -248,6 +252,64 @@ SPED-CLEAR_LEADER1996<br></br>
     
   </VerticalTimelineElement>
   
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '17px solid  rgb(33, 150, 243)' }}
+
+    date="2007 - 2008"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+ //  # icon={<WorkIcon />}
+  >
+    <h3 className="vertical-timeline-element-title">CLEAR LEADER AGAIN</h3>
+    <h4 className="vertical-timeline-element-subtitle">made for Ann Arbor Film Festival</h4>
+<p><h5>
+this year 700 feet instead                                                    
+of the usual 400 was donated and nearly all of it was                                                            
+enthusiastically filled up by the time it was shown.
+ </h5>
+</p>
+
+    <p>
+      <Popup trigger={<button className="button">SOUNDTRACKS</button>} modal>
+    <center>
+ The                                                    
+projectionists cut the film in half and projected both halves                                                              
+side-by-side on the wide screen.                                                                                           
+I had been composing soundtracks for them for 20 years.                                                                    
+Of course, never able to view the film before it's shown, I try to                                                         
+compose something that is rich enough with material that it might                                                          
+synchronize frequently with the stuff on the film (which is ALWAYS the                                                     
+same delightful sort of scribbling, stripy, wonderfully splotchy messy                                                     
+gibberish).                                                                                                                
+
+                                                                                                                           
+Most of the spoken bits were extracted from vintage instructional films        
+on proper maintenance of film projectors, thanks to Rick Prelinger's                                                       
+Internet Archive, the rest from The Tingler; Vernon, Florida and                                                           
+Donovan's Brain (Suspense Radio).                                                                                          
+Much of the sound collage was taken from radio collage done live on                                                        
+WCBN-FM 88.3 many years ago.  Some of it includes Barry Bouwsma's work                                                     
+when we were partners in audio crime during the first six years of                                                         
+Special Ed (that's the name of my radio show, not where the short bus                                                      
+took me).                                                                 
+<br></br>    
+    
+     2007_FFCL_Mix_18.35
+     <br></br>
+      <audio controls src="https://bothkinds7.web.app/2007_FFCL_Mix_18.35.mp3">
+</audio><br></br>  
+FFCL.2008.TIME<br></br>
+      <audio controls src="https://bothkinds7.web.app/FFCL.2008_TIME.mp3.mp3">
+</audio><br></br>
+
+</center>
+      </Popup>
+    </p>
+    
+  </VerticalTimelineElement>
+ 
+ 
   
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
