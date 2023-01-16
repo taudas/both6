1de7bebc750e44ad166d72ce05b3b4ad8b1e1d1d
taudas <taudas@gmail.com>
2023-01-16 14:00:47 -0500

    back to plain font and light grey

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,68 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import Script from 'next/script'
 import Head from 'next/head'
 import { Press_Start_2P}  from '@next/font/google'
+import { Inter}  from '@next/font/google'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
-const inter  = Press_Start_2P({ subsets: ['latin']
+const press  = Press_Start_2P({ subsets: ['latin']
 , weight: '400'
  })
-
+const inter  = Inter({ subsets: ['latin']
+, weight: '400'
+ })
 
 export const config = {  runtime: "experimental-edge",};
 export default function Index() {
   return (
   <>
 <Head>
-  <link href="https://vjs.zencdn.net/7.20.3/video-js.css" rel="stylesheet" />
+  <link href="https://vjs.zencdn.net/7.20.3/video-js.css" rel="stylesheet"/>
 </Head>
 
-<div className={inter.className} >
+<div className={inter.className}>
 <center>
-
 <VerticalTimeline>
-  <VerticalTimelineElement
+<VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
     contentArrowStyle={{ borderRight: '17px solid  rgb(33, 150, 243)' }}
     date="2023"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
- // {/*   icon={<WorkIcon />} */}
-  >
-    <h3 className="vertical-timeline-element-title">We Got Both Kinds</h3>
-    <h4 className="vertical-timeline-element-subtitle">this is a collection of the work of Ed Special</h4>
-    <p>
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}>
+<h3 className="vertical-timeline-element-title">We Got Both Kinds</h3>
+<h4 className="vertical-timeline-element-subtitle">this is a collection of the work of Ed Special</h4>
+<p>
 
-  <video   id="my-video"
-    className="video-js"
-    controls
-        poster="/flou.png"
-    preload="auto"
-audioonlymode="true"
+<video   id="my-video"
+className="video-js"
+controls
+poster="/flou.png"
+preload="auto"
 // width="320"
 //  height="64"
-    data-setup="{}">
-    <source src="https://bothkinds7.web.app/wegot.mp3" type="video/mp4" >
-    </source>
-    </video>
+data-setup="{}">
+<source src="https://bothkinds7.web.app/wegot.mp3" type="video/mp4">
+</source>
+</video>
+</p>
+    
+</VerticalTimelineElement>
 
-    </p>
-  </VerticalTimelineElement>
+
+
+
+
 
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
     contentArrowStyle={{ borderRight: '17px solid  rgb(33, 150, 243)' }}
     date="2020-2022"
-   iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+   iconStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
 //  #  icon={<WorkIcon />}
   >
     <h3 className="vertical-timeline-element-title">The Covid Years</h3>
@@ -73,10 +78,10 @@ The assembly of the "Warts & All" sampler was accomplished during lockdown
 
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
     contentArrowStyle={{ borderRight: '17px solid  rgb(33, 150, 243)' }}
     date="2006"
-   iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+   iconStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
 //  #  icon={<WorkIcon />}
   >
     <h3 className="vertical-timeline-element-title">Spacial Ed</h3>
@@ -138,10 +143,10 @@ SpacialEd20060629
 
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
     contentArrowStyle={{ borderRight: '17px solid  rgb(33, 150, 243)' }}
     date="1993"
-   iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+   iconStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
 //  #  icon={<WorkIcon />}
   >
     <h3 className="vertical-timeline-element-title">The Story Of The Naivete</h3>
@@ -168,11 +173,11 @@ SpacialEd20060629
 
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
     contentArrowStyle={{ borderRight: '17px solid  rgb(33, 150, 243)' }}
 
     date="2008"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
  //  # icon={<WorkIcon />}
   >
     <h3 className="vertical-timeline-element-title">Birdsongs </h3>
@@ -196,11 +201,11 @@ SpacialEd20060629
   
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
     contentArrowStyle={{ borderRight: '17px solid  rgb(33, 150, 243)' }}
 
     date="1988 - 1997"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
  //  # icon={<WorkIcon />}
   >
     <h3 className="vertical-timeline-element-title">CLEAR LEADER</h3>
@@ -254,11 +259,11 @@ SPED-CLEAR_LEADER1996<br></br>
   
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
     contentArrowStyle={{ borderRight: '17px solid  rgb(33, 150, 243)' }}
 
     date="2007 - 2008"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
  //  # icon={<WorkIcon />}
   >
     <h3 className="vertical-timeline-element-title">CLEAR LEADER AGAIN</h3>
@@ -313,10 +318,10 @@ FFCL.2008.TIME<br></br>
   
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
     contentArrowStyle={{ borderRight: '17px solid  rgb(33, 150, 243)' }}
     date="2002 - 2023"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
   // # icon={<SchoolIcon />}
   >
     <h3 className="vertical-timeline-element-title">More</h3>
@@ -331,17 +336,12 @@ FFCL.2008.TIME<br></br>
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
   // # icon={<StarIcon />}
   />
 </VerticalTimeline>
 
-
-
-
-
     </center>
-    
   <Script src="https://vjs.zencdn.net/7.20.3/video.min.js"></Script>
 </div>
 </>
